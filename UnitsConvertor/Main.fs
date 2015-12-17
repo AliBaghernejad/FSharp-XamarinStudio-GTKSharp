5152ecb721fdf9d4d51f62210ad739a425c0f0b8
Ali Baghernejad <AliBaghernezhad@gmail.com>
2015-12-17 19:50:22 +0330

    Project Files Added.

---
+++ UnitsConvertor/Main.fs
@@ -0,0 +1,75 @@
+﻿module Main = 
+    open System
+    open Gtk
+    open UnitsConvertor.AngleUnits
+    open UnitsConvertor
+
+    [<EntryPoint>]
+    let Main(args) = 
+        Application.Init()
+
+        // Create new widgets.
+        let win = new MainWindow.MyWindow()
+        let box = new VBox(false, 1)
+        let lblFrom = new Label("From:")
+        let cmbFrom = new ComboBox([|"Degree"; "Gradian"; "Radian"|])
+        let txtFrom = new Entry()
+        let lblTo = new Label("To:")
+        let cmbTo = new ComboBox([|"Degree"; "Gradian"; "Radian"|])
+        let txtTo = new Entry()
+
+        // Set widgets properties.
+        win.Title <- "Angle Units Convertor"
+        win.Resize(350,200)
+        lblFrom.Xalign <- 0.0f
+        lblTo.Xalign <- 0.0f
+        cmbFrom.Active <- 0
+        cmbTo.Active <- 0
+        txtTo.IsEditable <- false
+        txtFrom.Text <- "0"
+        txtTo.Text <- "0"
+
+        // A function to convert units.
+        let convertUnits() = 
+            try
+                let unitFrom = cmbFrom.ActiveText
+                let unitTo = cmbTo.ActiveText
+                let angle = txtFrom.Text
+                match unitFrom, unitTo with
+                | "Degree", "Gradian" -> 
+                        txtTo.Text <- (DegToGra (float angle * 1.0<deg>)).ToString()
+                | "Degree", "Radian"  -> 
+                        txtTo.Text <- (DegToRad (float angle * 1.0<deg>)).ToString()
+                | "Gradian", "Degree" -> 
+                        txtTo.Text <- (GraToDeg (float angle * 1.0<gra>)).ToString()
+                | "Gradian", "Radian" -> 
+                        txtTo.Text <- (GraToRad (float angle * 1.0<gra>)).ToString()
+                | "Radian", "Degree"  -> 
+                        txtTo.Text <- (RadToDeg (float angle * 1.0<rad>)).ToString()
+                | "Radian", "Gradian" -> 
+                        txtTo.Text <- (RadToGra (float angle * 1.0<rad>)).ToString()
+                | _                   -> 
+                        txtTo.Text <- txtFrom.Text 
+            with 
+                | exp -> txtTo.Text <- "Error! ;/"
+
+        // Events handle.
+        txtFrom.Changed.Add(fun _-> convertUnits())
+        cmbFrom.Changed.Add(fun _-> convertUnits())
+        cmbTo.Changed.Add(fun _-> convertUnits())
+
+        // Add widgets to main container.
+        box.Add(lblFrom)
+        box.Add(cmbFrom)
+        box.Add(txtFrom)
+        box.Add(lblTo)
+        box.Add(cmbTo)
+        box.Add(txtTo)
+
+        // Add main container to window.
+        win.Add(box)
+
+        // Draw anythings
+        win.ShowAll() 
+        Application.Run()
+        0 
---
+++ UnitsConvertor/UnitsOfMeasurefs.fs
@@ -0,0 +1,14 @@
+﻿namespace UnitsConvertor 
+open System 
+
+module AngleUnits = 
+    [<Measure>] type deg 
+    [<Measure>] type gra 
+    [<Measure>] type rad
+
+    let DegToGra (angle : float<deg>) = float angle * 1.11111111<gra> 
+    let DegToRad (angle : float<deg>) = (float angle * Math.PI / 180.0) * 1.0<rad> 
+    let GraToDeg (angle : float<gra>) = float angle * 0.9<deg> 
+    let GraToRad (angle : float<gra>) = float angle * 0.0157075<rad> 
+    let RadToGra (angle : float<rad>) = float angle * 63.6638548<gra> 
+    let RadToDeg (angle : float<rad>) = (float angle * 180.0 / Math.PI) * 1.0<deg>
